4b6308ee10a29f89bba16f2a60af1c7162587904
Tej Kumar <tej.0402@gmail.com>
2019-09-13 14:53:32 +0530

    Minor Changes

--- Install Solo.sh
+++ Install Solo.sh
@@ -4,7 +4,7 @@ red=`tput setaf 1`
 docker login
 docker run -d -p 8111:8080 -p 5003:5003 --name httpd httpd
 if [ $? -eq 0 ];then
-echo -e "\e[41m \e[1mInstalled httpd Successfully \e[0m"
+echo -e "\e[41m\e[1mInstalled httpd Successfully\e[0m"
 
 else
 echo -e "${red}\e[5mHttpd Installation Problem Occured!\e[25m\e[39m"
@@ -12,15 +12,15 @@ exit 1
 fi
 docker run -d -p 5111:5432 --name b postgres postgres:10
 if [ $? -eq 0 ];then
-echo -e "\e[41m \e[1mInstalled PostgreSQL Successfully \e[0m"
+echo -e "\e[41m\e[1mInstalled PostgreSQL Successfully\e[0m"
 
 else
-echo -e "${red}\e[5mPostgres Inatallation Problem Occured!\e[25m\e[39m"
+echo -e "${red}\e[5mPostgres Installation Problem Occured!\e[25m\e[39m"
 exit 1
 fi
 docker run -d -p 15111:15672 --name rabbitserver rabbitmq:3-management
 if [ $? -eq 0 ];then
-echo -e "\e[41m \e[1mInstalled RabbitMQ Successfully \e[0m"
+echo -e "\e[41m\e[1mInstalled RabbitMQ Successfully\e[0m"
 
 else
 echo -e "${red}\e[5mRabbitMQ Installation Problem Occured!\e[25m\e[39m"
